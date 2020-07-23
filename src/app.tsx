822719d13b1e58d0a0c35e20c3d3e88260fd4640
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-07-23 01:31:16 -0300

    bug fixed, started the Sign In page and fonts

--- src/app.tsx
+++ src/app.tsx
@@ -8,7 +8,10 @@ import Routes from './routes'
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="#312e38"
+    />
     <View style={{ flex: 1, backgroundColor: '#312e38' }}>
       <Routes />
     </View>
